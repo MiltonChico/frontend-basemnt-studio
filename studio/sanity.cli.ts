239228563de3ca083a4c3b3fda5e5964638ca227
Milton Chico <milton.chico@outlook.com>
2026-07-31 16:18:51 -0300

    fixed sanity files and deploy instructions

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -10,6 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
-    autoUpdates: true,
+    autoUpdates: false,
+    appId: 'vl6a8hxlcwvz16dmns45hpgn',
   },
 })
